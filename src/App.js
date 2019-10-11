0bd184bda9122ca29bfa848e96da523e1660ed4d
Grace Yang <yaycake@gmail.com>
2019-10-11 14:01:35 +0800

    whoops

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useEffect, Suspense } from 'react';
 import './App.css';
 // import Layout from './hoc/Layout'
+import * as actions from './store/actions/index'
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom'
+
+
+// const Artwork = React.lazy(()=> {
+//   return import('./containers/Artwork')
+// })
 
 function App() {
   return (
