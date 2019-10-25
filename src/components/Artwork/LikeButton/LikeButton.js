6aca349f56ee7d713326926b0db047d355de3db4
Grace Yang <yaycake@gmail.com>
2019-10-25 10:31:36 +0800

    added last changes

--- src/components/Artwork/LikeButton/LikeButton.js
+++ src/components/Artwork/LikeButton/LikeButton.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import styles from './LikeButton.module.css';
+import bookmarkLiked from '../../UI/Icons/bookmark_solid.svg'
+import bookmark from '../../UI/Icons/bookmark_outline.svg'
 
 const likeButton = ( props ) => {
     return (
-        <div onClick = {props.click} className={styles.LikeButton}>
+        <div onClick = {props.click} className={styles.LikeButton}
+        >
+            <img alt = "Bookmark this art" src={bookmark} className={styles.likeIcon} />
         </div>)
 };
 
